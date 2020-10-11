7644601a798d81d57a3ce01ebbf43a12cbdce6ef
unknown <mose23@naver.com>
2020-10-11 22:50:47 +0900

    working

--- server/app.js
+++ server/app.js
@@ -1,7 +1,39 @@
 import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import config from "./config";
+import cors from "cors";
+
+// Routes
+import postRoutes from "./routes/api/post";
+import userRoutes from "./routes/api/user";
+import authRoutes from "./routes/api/auth";
 
 const app = express();
+const { MONGO_URI } = config;
 
+app.use(hpp());
+app.use(helmet());
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongDB connecting Success !!"))
+  .catch((e) => console.log(e));
+
+// Use Routes
 app.get("/");
+app.use("/api/post", postRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
